f2c875f5698479f29c078ade2581ac8820378707
mhartman4 <mhartman4@gmail.com>
2017-08-26 08:38:39 -0400

    created new "chart" action
    TODO: create react components!

--- app/controllers/magnacarta_controller.rb
+++ app/controllers/magnacarta_controller.rb
@@ -1,4 +1,8 @@
 class MagnacartaController < ApplicationController
   def index
   end
+
+  def chart
+    render 'chart'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,11 @@ Rails.application.routes.draw do
   #root 'welcome#index', layout: "application"
   root 'magnacarta#index'
 
+  get '/chart/:chance_id' => 'magnacarta#chart'
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
 
   devise_for :users, controllers: {
-        sessions: 'users/sessions'
+      sessions: 'users/sessions'
   }
 
   namespace :api do
